a28693fd96ce8d3ef09ee5b96400cb7033a289b4
Denis Rasulev <ranklord@gmail.com>
2017-06-17 22:34:40 +0200

    refactoring

--- exploratory.r
+++ exploratory.r
@@ -4,6 +4,7 @@
 
 # load required libraries and functions
 library(tm)
+library(wordcloud2)
 suppressWarnings(library(ggplot2))
 suppressWarnings(library(lubridate))
 source("/Volumes/data/projects/fb_sentiment/parser.r")
@@ -13,29 +14,30 @@ setwd('/Volumes/data/projects/fb_sentiment/')
 comments_file  <- readLines("comments_processed.txt", encoding = "UTF-8",
                             ok = TRUE, skipNul = FALSE, warn = FALSE)
 
-# parse comments
-clean <- parse_comments(comments_file)
+# parse comments from prepared file
+df_comments <- parse_comments(comments_file)
 
 # split date column for further analysis
-clean[,'dt']    <- parse_date_time(clean[,'date'], orders = "mdy IMp")
-clean[,'year']  <- year(clean[,'dt'])
-clean[,'month'] <- month(clean[,'dt'])
-clean[,'day']   <- day(clean[,'dt'])
-clean[,'hour']  <- hour(clean[,'dt'])
+df_comments[,'dt']    <- parse_date_time(df_comments[,'date'], orders = "mdy IMp")
+df_comments[,'year']  <- year(df_comments[,'dt'])
+df_comments[,'month'] <- month(df_comments[,'dt'])
+df_comments[,'day']   <- day(df_comments[,'dt'])
+df_comments[,'hour']  <- hour(df_comments[,'dt'])
 
 # remove unused columns
-clean[,c('date','dt')] <- NULL
+df_comments[,c('date','dt')] <- NULL
 
 # Exploratory Analysis 1
 # ==============================================================================
 
-# top commenter by number of comments
-w <- table(clean$name)
+# who is top commenter by number of comments
+w <- table(df_comments$name)
 t <- as.data.frame(w)
 t <- t[order(t$Freq, decreasing = TRUE),]
 names(t)[1] = 'Name'
 names(t)[2] = 'Comments'
 
+# show top commenters as bar plot
 par(mar = c(3,10,2,1), adj = 0)
 barplot(t$Comments[1:30],
         main = "Top 30 commenters",
@@ -48,67 +50,64 @@ barplot(t$Comments[1:30],
         las = 1)
 
 # most liked comment/commenter
-v <- clean[order(clean$like, decreasing = TRUE),]
+v <- df_comments[order(df_comments$like, decreasing = TRUE),]
 
-#par(mar = c(3,10,2,1), adj = 0)
+# show top most liked as bar plot
+par(mar = c(3,12,2,1), adj = 0)
 barplot(v$like[1:30],
-        main = "Top 30 most liked",
+        main = "Top 30 most liked commenters",
         names.arg = v$name[1:30],
         col = rainbow(45),
         xlim = c(0,100),
         ylim = c(35,0),
         horiz = TRUE,
-        line = -0.1,
+        line = 0,
         las = 1)
 
-# most lengthy comment - max(nchar(clean$cmnt))
+# most lengthy comment - max(nchar(df_comments$cmnt))
 comment_length = 0
-for (i in 1:nrow(clean)) {
-     if (nchar(clean$cmnt[i]) > comment_length) {
-          comment_length <- nchar(clean$cmnt[i])
+for (i in 1:nrow(df_comments)) {
+     if (nchar(df_comments$cmnt[i]) > comment_length) {
+          comment_length <- nchar(df_comments$cmnt[i])
           index <- i
      }
 }
-sprintf("Author of the most lengthy comment is %s", clean$name[index])
-number_of_words <- sapply(gregexpr("\\W+", clean$cmnt[index]), length) + 1
+sprintf("Author of the most lengthy comment is %s", df_comments$name[index])
+number_of_words <- sapply(gregexpr("\\W+", df_comments$cmnt[index]), length) + 1
 sprintf("The comment contains %d characters and %d words",
-        nchar(clean$cmnt[index]), number_of_words)
+        nchar(df_comments$cmnt[index]), number_of_words)
 
 # Exploratory Analysis 2
 # ==============================================================================
 
-dfCorpus = Corpus(VectorSource(clean$cmnt))
+# create text corpus
+df_corpus = Corpus(VectorSource(df_comments$cmnt), readerControl = list(language = "rus"))
 
-dfCorpus <- tm_map(dfCorpus, tolower)
-dfCorpus <- tm_map(dfCorpus, removeNumbers)
-dfCorpus <- tm_map(dfCorpus, removePunctuation)
-dfCorpus <- tm_map(dfCorpus, removeWords, stopwords('russian'))
-dfCorpus <- tm_map(dfCorpus, removeWords, c("вы", "её", "не", "это", "жок",
+# pre-process corpus
+df_corpus <- tm_map(df_corpus, content_transformer(tolower))
+df_corpus <- tm_map(df_corpus, removeNumbers)
+df_corpus <- tm_map(df_corpus, removePunctuation)
+df_corpus <- tm_map(df_corpus, removeWords, stopwords('russian'))
+df_corpus <- tm_map(df_corpus, removeWords, c("вы", "её", "не", "это", "жок",
                                             "деп", "фото", "екен", "photo",
                                             "emoticon"))
-dfCorpus <- tm_map(dfCorpus, stripWhitespace)
-#dfCorpus <- tm_map(dfCorpus, stemDocument)
-#dfCorpus <- tm_map(dfCorpus, PlainTextDocument)
+df_corpus <- tm_map(df_corpus, stripWhitespace)
 
-tdm <- TermDocumentMatrix(dfCorpus)
+# create term-document matrix
+tdm <- TermDocumentMatrix(df_corpus)
 
+# create data frame with words sorted by frequency
 v <- sort(rowSums(as.matrix(tdm)), decreasing = TRUE)
 d <- data.frame(word = names(v), freq = v)
-#d <- d[order(-d[,2]),]
+
+# check top 10 words
 head(d, 10)
 
+# build word cloud
 set.seed(12345)
-
-# library(wordcloud)
-# wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-#           max.words = 200, random.order = FALSE, rot.per = 0.35,
-#           colors = brewer.pal(8, "Dark2"))
-
-library(wordcloud2)
 wordcloud2(data = d)
 
-findFreqTerms(tdm, lowfreq = 100)
-
+# show top words as bar plot
 par(mar = c(3,6,2,1), adj = 0)
 barplot(d[1:30,]$freq,
         main = "Top 30 words",
