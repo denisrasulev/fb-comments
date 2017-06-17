c2367be2bef14c3030ac91c1380ec941040ca07b
Denis Rasulev <ranklord@gmail.com>
2017-06-17 21:27:37 +0200

    comments updated, wordcloud added

--- exploratory.r
+++ exploratory.r
@@ -3,13 +3,15 @@
 # All Rights Reserved.
 
 # load required libraries and functions
+library(tm)
 suppressWarnings(library(ggplot2))
 suppressWarnings(library(lubridate))
 source("/Volumes/data/projects/fb_sentiment/parser.r")
 
 # read comments file
 setwd('/Volumes/data/projects/fb_sentiment/')
-comments_file  <- readLines("comments_processed.txt", encoding = "UTF-8", ok = TRUE, skipNul = FALSE, warn = FALSE)
+comments_file  <- readLines("comments_processed.txt", encoding = "UTF-8",
+                            ok = TRUE, skipNul = FALSE, warn = FALSE)
 
 # parse comments
 clean <- parse_comments(comments_file)
@@ -25,35 +27,38 @@ clean[,'hour']  <- hour(clean[,'dt'])
 clean[,c('date','dt')] <- NULL
 
 # Exploratory Analysis 1
+# ==============================================================================
 
 # top commenter by number of comments
-w = table(clean$name)
-t = as.data.frame(w)
+w <- table(clean$name)
+t <- as.data.frame(w)
 t <- t[order(t$Freq, decreasing = TRUE),]
 names(t)[1] = 'Name'
 names(t)[2] = 'Comments'
 
-par(mai = c(.6,2,.1,.5))
+par(mar = c(3,10,2,1), adj = 0)
 barplot(t$Comments[1:30],
+        main = "Top 30 commenters",
+        names.arg = t$Name[1:30],
         col = rainbow(45),
         xlim = c(0,300),
         ylim = c(35,0),
-        names.arg = t$Name[1:30],
-        #cex.names = 0.8,
         horiz = TRUE,
+        line = 0,
         las = 1)
 
 # most liked comment/commenter
 v <- clean[order(clean$like, decreasing = TRUE),]
 
-par(mai = c(.6,2.5,.1,.5))
+#par(mar = c(3,10,2,1), adj = 0)
 barplot(v$like[1:30],
+        main = "Top 30 most liked",
+        names.arg = v$name[1:30],
         col = rainbow(45),
         xlim = c(0,100),
         ylim = c(35,0),
-        names.arg = v$name[1:30],
-        #cex.names = 0.9,
         horiz = TRUE,
+        line = -0.1,
         las = 1)
 
 # most lengthy comment - max(nchar(clean$cmnt))
@@ -64,7 +69,55 @@ for (i in 1:nrow(clean)) {
           index <- i
      }
 }
-sprintf("Author of most lengthy comment is %s", clean$name[index])
-sprintf("Comments contains %d characters", nchar(clean$cmnt[index]))
+sprintf("Author of the most lengthy comment is %s", clean$name[index])
+number_of_words <- sapply(gregexpr("\\W+", clean$cmnt[index]), length) + 1
+sprintf("The comment contains %d characters and %d words",
+        nchar(clean$cmnt[index]), number_of_words)
 
 # Exploratory Analysis 2
+# ==============================================================================
+
+dfCorpus = Corpus(VectorSource(clean$cmnt))
+
+dfCorpus <- tm_map(dfCorpus, tolower)
+dfCorpus <- tm_map(dfCorpus, removeNumbers)
+dfCorpus <- tm_map(dfCorpus, removePunctuation)
+dfCorpus <- tm_map(dfCorpus, removeWords, stopwords('russian'))
+dfCorpus <- tm_map(dfCorpus, removeWords, c("вы", "её", "не", "это", "жок",
+                                            "деп", "фото", "екен", "photo",
+                                            "emoticon"))
+dfCorpus <- tm_map(dfCorpus, stripWhitespace)
+#dfCorpus <- tm_map(dfCorpus, stemDocument)
+#dfCorpus <- tm_map(dfCorpus, PlainTextDocument)
+
+tdm <- TermDocumentMatrix(dfCorpus)
+
+v <- sort(rowSums(as.matrix(tdm)), decreasing = TRUE)
+d <- data.frame(word = names(v), freq = v)
+#d <- d[order(-d[,2]),]
+head(d, 10)
+
+set.seed(12345)
+
+# library(wordcloud)
+# wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+#           max.words = 200, random.order = FALSE, rot.per = 0.35,
+#           colors = brewer.pal(8, "Dark2"))
+
+library(wordcloud2)
+wordcloud2(data = d)
+
+findFreqTerms(tdm, lowfreq = 100)
+
+par(mar = c(3,6,2,1), adj = 0)
+barplot(d[1:30,]$freq,
+        main = "Top 30 words",
+        names.arg = d$word[1:30],
+        #col = c("lightblue","red","green"),
+        col = "lightgreen",
+        #col = rainbow(90),
+        xlim = c(0,450),
+        ylim = c(35,0),
+        horiz = TRUE,
+        line = 0,
+        las = 1)
--- parser.r
+++ parser.r
@@ -1,13 +1,14 @@
-# Facebook comments Parser
+# Parser for Comments from pre-Processed File
 # (c) 2017 Denis Rasulev
 # All Rights Reserved.
 
 parse_comments <- function(comments) {
-     # this function goes through comments file line by line, finds and saves
-     # to data frame - name, text of comment, number of likes and date posted
-     # returns data.frame['name','cmnt','like','date']
+     # this function goes through pre-processed comments file row by row,
+     # finds information by certain markers and saves it to data frame as
+     # name, text of comment, number of likes and date posted
+     # then returns data.frame['name','cmnt','like','date']
 
-     # save length of comments file
+     # save length of file with comments
      number_of_rows <- length(comments)
 
      # prepare empty data frame to store name, comment, likes and date
@@ -15,22 +16,25 @@ parse_comments <- function(comments) {
      colnames(df) <- c('name','cmnt','like','date')
 
      for (i in 1:number_of_rows ) {
+
+          # if row is empty...
           if ( comments[i] == "" ) {
 
-               # first string after empty one is commenter's name
+               # then next row after empty one contains commenter's name
                df[i, 'name'] <- comments[i + 1]
 
-               # second string after empty one is text of a comment
-               # it starts with name of a commenter so we remove it
+               # third row after empty one contains text of a comment and
+               # it always starts with the name of a commenter so we remove it
                comment_text <- sub(paste0(comments[i + 1],' '), '', comments[i + 2])
 
-               # it may consist of several line so we need index to read many lines
+               # text of a comment may be on several lines so we need index
+               # to read them all
                j <- 3
 
                # while next line doesn't start with middle dot '·' (unicode 00B7)
                while (substring(comments[i + j], 1, 1) != '\U00B7') {
 
-                    # in case we reach end of file we need to break the loop
+                    # if we reach end of file we need to break the loop
                     if ( i + j > number_of_rows ) {
                          break
                     }
@@ -40,13 +44,13 @@ parse_comments <- function(comments) {
                     j <- j + 1
                }
 
-               # save text of a comment
+               # save complete text of a comment
                df[i, 'cmnt'] <- comment_text
 
                # save number of likes for a comment, removing midle dot
                df[i, 'like'] <- sub('\U00B7 ', '', comments[i + j])
 
-               # save a date when comment was posted, removing midle dot
+               # save date when a comment was posted, removing midle dot
                df[i, 'date'] <- sub('\U00B7 ', '', comments[i + j + 1])
           }
      }
